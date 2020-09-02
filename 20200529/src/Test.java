79d50cde669fc675f74644b95fad441cb7b04f7f
sjc <1508445842@qq.com>
2020-09-02 12:14:00 +0800

    1

---
+++ 20200529/src/Test.java
@@ -0,0 +1,21 @@
+/*
+增加列：alter table tableName add columnName varchar (30)
+删除列：alter table tableName drop column columnName
+
+ SQLite，是一款轻型的 关系型 数据库， 它包含在一个相对小的C库中。
+ 它是D.RichardHipp建立的公有领域项目。它的设计目标是 嵌入式 的，
+ 而且目前已经在很多 嵌入式产品 中使用了它，占用资源非常的低，在嵌入式设备中，
+ 可能只需要几百K的内存
+ 开发Android应用程序用轻量级数据库SQLite。
+
+ 索引：
+ https://www.nowcoder.com/questionTerminal/413d6a33d7a44169812f3613923f206a?from=14pdf
+
+ where增加分组前的限定，having增加分组后的限定
+ where后不允许跟统计函数。WHERE字句中是不能用聚集表达式作为条件表达式的~
+
+ 赋予权限的SQL语句：GRANT [权限] ON [TABLE] TO 'USERNAME'@'LOCALHOST';
+ */
+public class Test {
+    
+}
